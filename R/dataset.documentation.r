df70cea9f04ed2952754253a219e86119cb2069b
Daniel Duplisea <daniel.duplisea@gmail.com>
2019-12-25 01:59:21 -0500

    Dec structure

--- R/dataset.documentation.r
+++ R/dataset.documentation.r
@@ -19,8 +19,42 @@
 #' @seealso field.description
 #' @name EA.data
 #' @usage EA.data
-#' @format data.table
-#' @author Peter Galbraith, Marjolaine Blais
+#' @format data.table, data.frame
+#' @author Denis Bernier, Peter Galbraith, Marjolaine Blais
+#' @references
+#'         Blais, M., Galbraith, P.S., Plourde, S., Scarratt, M., Devine, L. and Lehoux, C. 2019. Chemical and
+#'         Biological Oceanographic Conditions in the Estuary and Gulf of St. Lawrence during 2017. DFO Can. Sci.
+#'         Advis. Sec. Res. Doc. 2019/009. iv + 56 pp.
+#'
+#'         Galbraith, P.S., Chassé, J., Nicot, P., Caverhill, C., Gilbert, D., Pettigrew, B., Lefaivre,
+#'         D., Brickman, D., Devine, L., and Lafleur, C. 2015. Physical Oceanographic Conditions in the Gulf of
+#'         St. Lawrence in 2014. DFO Can. Sci. Advis. Sec. Res. Doc. 2015/032. v + 82 p
+#'         http://www.dfo-mpo.gc.ca/csas-sccs/Publications/ResDocs-DocRech/2015/2015_032-eng.html
+#'
+
+NULL
+
+
+#' Variable descriptions in EA.data
+#'
+#' Descriptions of the variables in EA.data, the units of measure and other information
+#'
+#' \itemize{
+#'   \item variable The name of the variable in EA.data
+#'   \item description A description of the variable
+#'   \item units The units of measure for the value of the variable
+#'   \item contact The name of the contact person for each variable
+#'   \item type The broad category of the variabl, e.g. "physical", "chemical", "planktonic", "phenological"
+#'   \item extraction.date The date which the data were extracted from the parent data base by the person named as the contact
+#' }
+#'
+#' @docType data
+#' @keywords ecosystem approach data
+#' @seealso field.description
+#' @name variable.description
+#' @usage variable.description
+#' @format data.table, data.frame
+#' @author Daniel Duplisea
 NULL
 
 
@@ -40,7 +74,7 @@ NULL
 #' @keywords datasets
 #' @name field.description
 #' @usage field.description
-#' @format data.table
+#' @format data.table, data.frame
 #' @author Daniel Duplisea
 NULL
 
@@ -65,6 +99,35 @@ NULL
 #'             Miller, R. et Chabot. D. 2014. Code list of marine plants, invertebrates and vertebrates used by the Quebec Region of DFO. Canadian Data Report of Fisheries and Aquatic Sciences 1254 : iv+ 115 p.
 #' @name field.description
 #' @usage field.description
-#' @format data.table
+#' @format data.table, data.frame
 #' @author Daniel Duplisea
 NULL
+
+
+#' Gulf of St Lawrence Ecosystem Approach R package
+#'
+#' @description To get started you probably want some kind of metadata description:
+#'
+#'                   metadata.f(verbosity="low")
+#'
+#'
+#'              You may be interested in what variables are available:
+#'
+#'                   vars.f(variable.type="all")
+#'
+#'              You may be interested in what variables are available of a particular type:
+#'
+#'                   vars.f(variable.type="chemical")
+#'
+#'
+#'              You may want to explore data with simple plots:
+#'
+#'                   EA.plot.f(years=1900:2030, variables=c("T150", "ph_bot.fall", "T250"), EARs=1:4, type="b",pch=20)
+#'
+#'
+#'              You may just want to extract the data from the above plot:
+#'
+#'                   EA.query.f(variables=c("T150", "ph_bot.fall", "T250"), years=1900:2030, EARs=1:4)
+#' @export
+
+gslea= function(){}
--- R/functions.r
+++ R/functions.r
@@ -1,192 +1,116 @@
-
-
-
-#' a map of the Gulf of St. Lawrence with ecoregions and options to include survey points and bathymetry
+#' Metadata with three levels of verbosity
 #'
-#' @param survey.points logical. Plot the fish survey points from all years from the set file
-#' @param pch the point type for depicting fish survey points
-#' @param bathymetry show bathymetry lines
-#' @param isobath bamthymetric depths to show
-#' @param bathycols a vector of bathymetric colour, recycled
-#' @description A map of the Gulf of St Lawrence showing the ecoregions and with options to plot different
-#'        bathymetric contours and fish survey points
+#' @param verbosity "low","med","high".
+#' @description  Pulls out metadata from the various datasets to give an overview. "med" and "high" verbosity level data
+#'               may need further display formatting to get a good overview. Try the library 'formattable' and run that
+#'               function on the higher verbosity metadata to get a nicer looking table that you can make sense of.
+#' @author Daniel Duplisea
 #' @export
-gslea.map.f= function(xlim=c(-70,-55), ylim=c(42,53),
-  bathymetry=F, isobath=c(150,300), bathycols= c("yellow","blue")){
-  #data prep
-  data("worldLLhigh")
-  gsl= clipPolys(worldLLhigh, xlim = c(286, 310), ylim = c(43, 53)) #clip the world to gsl polyset
-  gsl$X= gsl$X-360
-  pdata = calcCentroid(EcoRegions)
-  pdata$col = grey.colors(9)#1:9#paste0("grey",11:19)
-  pdata$label = c("1-NW","2-NE","3-Centre","4-Mecatina","5-MS","6-NS","7-LH","10/1-Est","50-BdC")
-  pdata = as.PolyData(pdata, projection = "LL")
-
-  #plot the ecoregions then the land then the bathymetry
-  plotMap(EcoRegions,col="lightblue",bg=rgb(224,253,254,maxColorValue=255),polyProps = pdata,
-    xlim=xlim,ylim=ylim,las=1,xaxt="n", yaxt="n",xlab="",ylab="")
-  xint= seq(xlim[1],xlim[2],length=5)
-  yint= seq(ylim[1],ylim[2],length=6)
-  mtext("Longitude west",side=1,line=3)
-  mtext("Latitude north",side=2,line=3)
-  axis(1, at=xint, labels=xint*-1, lty=1,lwd=1,lwd.ticks= 1, cex.axis=.7)
-  axis(2, at=yint, labels=yint*1, lty=1,lwd=1,lwd.ticks=1,las=1, cex.axis=.7)
-  addPolys(EcoRegions, polyProps = pdata)
-  addPolys(gsl,col="tan")
-
-  #add depth contours
-  if(bathymetry){
-    ocCL = contourLines(ocBathy, levels=isobath)
-    ocCP = convCP(ocCL, projection = "LL")
-    ocPoly = ocCP$PolySet
-    addLines(thinPolys(ocPoly, tol=1,filter = 5), col =bathycols)
-    legend("bottomright", bty = "n", col = bathycols, lwd = 1, legend = as.character(isobath), inset = 0.05,
-    title = "Isobaths (m)",cex=0.7)
-  }
-  boxtext(pdata$X, pdata$Y, labels = pdata$label,cex=0.7,col.bg="beige")
-}
-
-
-
-#' Add text with background box to a plot
-#'
-#' \code{boxtext} places a text given in the vector \code{labels}
-#' onto a plot in the base graphics system and places a coloured box behind
-#' it to make it stand out from the background.
-#'
-#' @param x numeric vector of x-coordinates where the text labels should be
-#' written. If the length of \code{x} and \code{y} differs, the shorter one
-#' is recycled.
-#' @param y numeric vector of y-coordinates where the text labels should be
-#' written.
-#' @param labels a character vector specifying the text to be written.
-#' @param col.text the colour of the text
-#' @param col.bg color(s) to fill or shade the rectangle(s) with. The default
-#' \code{NA} means do not fill, i.e., draw transparent rectangles.
-#' @param border.bg color(s) for rectangle border(s). The default \code{NA}
-#' omits borders.
-#' @param adj one or two values in [0, 1] which specify the x (and optionally
-#' y) adjustment of the labels.
-#' @param pos a position specifier for the text. If specified this overrides
-#' any adj value given. Values of 1, 2, 3 and 4, respectively indicate
-#' positions below, to the left of, above and to the right of the specified
-#' coordinates.
-#' @param offset when \code{pos} is specified, this value gives the offset of
-#' the label from the specified coordinate in fractions of a character width.
-#' @param padding factor used for the padding of the box around
-#' the text. Padding is specified in fractions of a character width. If a
-#' vector of length two is specified then different factors are used for the
-#' padding in x- and y-direction.
-#' @param cex numeric character expansion factor; multiplied by
-#' code{par("cex")} yields the final character size.
-#' @param font the font to be used
-#'
-#' @return Returns the coordinates of the background rectangle(s). If
-#' multiple labels are placed in a vactor then the coordinates are returned
-#' as a matrix with columns corresponding to xleft, xright, ybottom, ytop.
-#' If just one label is placed, the coordinates are returned as a vector.
-#' @author Ian Kopacka
 #' @examples
-#' ## Create noisy background
-#' plot(x = runif(1000), y = runif(1000), type = "p", pch = 16,
-#' col = "#40404060")
-#' boxtext(x = 0.5, y = 0.5, labels = "some Text", col.bg = "#b2f4f480",
-#'     pos = 4, font = 2, cex = 1.3, padding = 1)
+#' metadata.f("low")
+#' data.desc= metadata.f("med"); library(formattable); formattable(data.desc$Descriptions, align="l")
+metadata.f= function(verbosity="low"){
+  if(verbosity=="low"){
+    desc= list(
+      Number.of.variables= EA.data[,uniqueN(variable)],
+      Number.of.EARS= EA.data[,uniqueN(EAR)],
+      Number.of.years= EA.data[,uniqueN(year)],
+      Number.of.observations= nrow(EA.data))
+  }
+  if(verbosity=="med"){
+      Unique.variables= data.table(variable=EA.data[,unique(variable)],key="variable")
+      Variables.with.descriptions= Unique.variables[variable.description[,.(variable, description, units)]]
+#      formattable(Variables.with.descriptions,align="l")
+      desc= list(Unique.Variables= Unique.variables,Descriptions=Variables.with.descriptions)
+  }
+  if(verbosity=="high"){
+      Unique.variables= data.table(variable=EA.data[,unique(variable)],key="variable")
+      Variables.with.descriptions= Unique.variables[variable.description[,.(variable, description, units)]]
+#      formattable(Variables.with.descriptions,align="l")
+      desc= list(Unique.Variables= Unique.variables,Descriptions=Variables.with.descriptions)
+  }
+  if(verbosity=="vangogh"){
+    desc= list(Number.of.EARS= 1)
+  }
+  desc
+}
+
+#' Show  variables available in the data set
+#' @param type The type of data you are interested in. Options: "all","physical","chemical","planktonic","phenological","fish"
+#' @description  Lists all the variables in the database by data type
+#' @author Daniel Duplisea
 #' @export
-boxtext <- function(x, y, labels = NA, col.text = NULL, col.bg = NA,
-        border.bg = NA, adj = NULL, pos = NULL, offset = 0.5,
-        padding = c(0.5, 0.5), cex = 1, font = graphics::par('font')){
+#' @examples vars.f("phenological")
+#' #consider using the library formattable to make a readable table that prints to the plot window
+#' tmp= vars.f("all"); formattable(tmp)
+vars.f= function(variable.type="all"){
+  if (variable.type=="all"){
+    vars= variable.description[,.(variable, type, description, units)]
+  }
+  if (variable.type!="all"){
+    vars= variable.description[ type %in% variable.type, .(variable, type, description, units)]
+  }
+  vars= as.data.frame(as.matrix(vars,ncol=1))
+  vars
+}
 
-    ## The Character expansion factro to be used:
-    theCex <- graphics::par('cex')*cex
 
-    ## Is y provided:
-    if (missing(y)) y <- x
-
-    ## Recycle coords if necessary:
-    if (length(x) != length(y)){
-        lx <- length(x)
-        ly <- length(y)
-        if (lx > ly){
-            y <- rep(y, ceiling(lx/ly))[1:lx]
-        } else {
-            x <- rep(x, ceiling(ly/lx))[1:ly]
-        }
-    }
-
-    ## Width and height of text
-    textHeight <- graphics::strheight(labels, cex = theCex, font = font)
-    textWidth <- graphics::strwidth(labels, cex = theCex, font = font)
-
-    ## Width of one character:
-    charWidth <- graphics::strwidth("e", cex = theCex, font = font)
-
-    ## Is 'adj' of length 1 or 2?
-    if (!is.null(adj)){
-        if (length(adj == 1)){
-            adj <- c(adj[1], 0.5)
-        }
-    } else {
-        adj <- c(0.5, 0.5)
-    }
-
-    ## Is 'pos' specified?
-    if (!is.null(pos)){
-        if (pos == 1){
-            adj <- c(0.5, 1)
-            offsetVec <- c(0, -offset*charWidth)
-        } else if (pos == 2){
-            adj <- c(1, 0.5)
-            offsetVec <- c(-offset*charWidth, 0)
-        } else if (pos == 3){
-            adj <- c(0.5, 0)
-            offsetVec <- c(0, offset*charWidth)
-        } else if (pos == 4){
-            adj <- c(0, 0.5)
-            offsetVec <- c(offset*charWidth, 0)
-        } else {
-            stop('Invalid argument pos')
-        }
-    } else {
-      offsetVec <- c(0, 0)
-    }
-
-    ## Padding for boxes:
-    if (length(padding) == 1){
-        padding <- c(padding[1], padding[1])
-    }
-
-    ## Midpoints for text:
-    xMid <- x + (-adj[1] + 1/2)*textWidth + offsetVec[1]
-    yMid <- y + (-adj[2] + 1/2)*textHeight + offsetVec[2]
-
-    ## Draw rectangles:
-    rectWidth <- textWidth + 2*padding[1]*charWidth
-    rectHeight <- textHeight + 2*padding[2]*charWidth
-    graphics::rect(xleft = xMid - rectWidth/2,
-            ybottom = yMid - rectHeight/2,
-            xright = xMid + rectWidth/2,
-            ytop = yMid + rectHeight/2,
-            col = col.bg, border = border.bg)
-
-    ## Place the text:
-    graphics::text(xMid, yMid, labels, col = col.text, cex = theCex, font = font,
-            adj = c(0.5, 0.5))
-
-    ## Return value:
-    if (length(xMid) == 1){
-        invisible(c(xMid - rectWidth/2, xMid + rectWidth/2, yMid - rectHeight/2,
-                        yMid + rectHeight/2))
-    } else {
-        invisible(cbind(xMid - rectWidth/2, xMid + rectWidth/2, yMid - rectHeight/2,
-                        yMid + rectHeight/2))
-    }
+#' Query the data
+#'
+#' @param variables variable vector
+#' @param years year vector
+#' @param EARs EAR vector
+#' @tabular do you want the data output with few colums and repeated rows or a a more tabular format (often what you want for plotting in excel)
+#' @description  creates a data.table based on the variables you selected. data.tables inherit data.frame
+#' @author Daniel Duplisea
+#' @export
+#' @examples
+#' EA.query.f(years=1999:2012, variables=c("T150", "ph_bot.fall", "T250"), EARs=1:2)
+EA.query.f= function(variables, years, EARs){
+ warning("GENERAL WARNING ABOUT FISH SURVEY DATA: there were fish survey vessel and gear changes in 1990 and 2004. New survey strata were added in 2007 and biodiversity protocols were changed in 2006. Careful with fish data intepretation spanning these years.", noBreaks.=T,immediate.=T)
+ out= EA.data[variable %in% variables & year %in% years & EAR %in% EARs]
+ out
 }
 
 
 
+#' Plot data time series
+#'
+#' @param variables variable vector
+#' @param years year vector
+#' @param EARs EAR vector
+#' @param ... arguments to par for plotting
+#' @description  Plots a matrix of variables with EAR as columns and variable as rows. If no data then a blank graph
+#'               is plotted.
+#' @author Daniel Duplisea
+#' @export
+#' @examples
+#' EA.plot.f(years=1900:2030, variables=c("T150", "ph_bot.fall", "T250"), EARs=1:4, type="b",pch=20)
+EA.plot.f= function(variables, years, EARs, ...){
+  dat= EA.query.f(variables=variables, years=years, EARs=EARs)
+  actual.EARs= sort(as.numeric(dat[,unique(EAR)]))
+  no.plots= length(variables)*length(actual.EARs)
+  if(no.plots>25) warning("this query generates more than 25 plots, consider fewer variables or EARs")
+  par(mfcol=c(length(variables), length(actual.EARs)),mar=c(1.3,2,3.2,1),omi=c(.1,.1,.1,.1))
+  counter=1
+  for(i in actual.EARs){
+    ear.dat= dat[EAR==i]
+    for(ii in 1:length(variables)){
+      var.dat= ear.dat[variable==variables[ii]]
+      if(nrow(var.dat)<1) plot(0, xlab="", ylab="", xaxt="n",yaxt="n",main=paste("EAR",i,variables[ii]), ...)
+      if(nrow(var.dat)>0) plot(var.dat$year, var.dat$value, xlab="", ylab="", main=paste("EAR",i,variables[ii]), ...)
+    }
+    counter= counter+1
+  }
+}
 
-#years=2003:2005; variables=c("T150", "ph_bot.fall")
+#EA.plot.f(years=1900:2030, variables=c("T150", "ph_bot.fall", "T250"), EARs=1:4, type="b",pch=20)
 
-#extract.f= function(years, EARs, variables, verbosity=1){
- # EA.data[.(year %in% years),variable %in% variables,c(year,EAR)]
+#EA.plot.f(years=1900:2030, variables=c("T150", "ph_bot.fall", "T250"), EARs=1:50, type="b",pch=20)
+
+#EA.plot.f(years=1900:2030, variables=c("T150", "ph_bot.fall", "T250","ice.max","chl0_100.annual","chl0_100.early_summer"),EARs=1:50, type="b",pch=20,cex.main=.8)
+
+
+
+#one will want to query by data type
+# create a multipage plot option
